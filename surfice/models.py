88e41ef3e4cf147e6fb3f877172e94122f15dd79
natewinck <gotmacs@gmail.com>
2014-06-17 16:29:29 -0400

    Skeleton created

--- surfice/models.py
+++ surfice/models.py
@@ -2,6 +2,33 @@ from django.db import models
 
 # Create your models here.
 
+
+
+# -----------------------------------------
+# Status Class (could be in a separate app)
+#
+# Dictates the status of a generic object.  It [can optionally]
+# connect with the a generic status image class NO IT CAN'T
+# 
+# CLASS VARIABLES
+# name			Name of the status e.g. "Totally axed" or "choppy" or "clean"
+# description	Description of the status
+#
+# CLASS METHODS
+######void set_default()		Set this status as the default
+# -----------------------------------------
+class Status(models.Model):
+	# Class variables
+	name 		= models.CharField(max_length=512, unique=True)
+	description = models.TextField()
+	
+	def __unicode__(self):
+		return self.name
+
+
+
+
+
 # --------------
 # Surf Class
 # Group container for a set of Surfices
@@ -9,8 +36,8 @@ from django.db import models
 
 class Surf(models.Model):
 	# Class variables
-	name = models.CharField(max_length=128, unique=True)
-	description = models.CharField(unique=False)
+	name = models.CharField(max_length=512, unique=True)
+	description = models.TextField()
 	
 	# Class methods
 	def __unicode__(self):
@@ -25,21 +52,78 @@ class Surf(models.Model):
 #				group
 # group			Which Surf group this belongs to. Defaults to NULL
 # description	Description of the service
+# timestamp		Set automatically the first time the surfice is created
 
 class Surfice(models.Model):
 	# Class variables
-	name = models.CharField(unique=True)
-	group = models.ForeignKey(Surf)
-	description = models.CharField(unique=False)
+	name 		= models.CharField(max_length=512, unique=True)
+	group 		= models.ForeignKey(Surf)
+	description = models.TextField()
+	timestamp	= models.DateField(auto_now=False, auto_now_add=True)
+	
+	# Status is part of the model
+	status = models.ForeignKey(Status) ##
+	
+	def __unicode__(self):
+		return self.name
+	
+	def __init__(self):
+		self.name = "Default"
+		self.description = "Default description"
+		self.group = Surf()
+	
+
+
+# -----------------------------------------
+# Ding Class
+#
+# Any object of type Ding is an issue.  This just captures the
+# initial issue and then the closing of the ding (issue).
+# Any usage of the ding will either need to be added here or
+# or added in a different app
+#
+# CLASS VARIABLES
+# timestamp			timestamp of the original issue
+# service_id		which "surfice" is having the issue
+# name				Name of the user
+#
+class Ding(models.Model):
+	# Class variables
+	name		= models.CharField(max_length=512, unique=False)
+	timestamp	= models.DateField(auto_now=False, auto_now_add=True)
+	service_id	= models.IntegerField()
+	email		= models.EmailField()
+	description	= models.TextField()
 	
 	def __unicode__(self):
 		return self.name
 
+	
 
-# Events Class
-
-# Statuses Class (could be in a separate app)
-
-# Issues Class (different app!)
 # Includes information from users of services that they submit.
 # Includes methods that handle the various tasks related to tickets
+
+
+
+# --------------------------------------
+# Event Class
+#
+# A class for recording events of a general object
+# and what the current status is.
+#
+# CLASS VARIABLES
+# timestamp			timestamp of the event
+# status			Status object
+# service_id		id of the service
+# description		description of the event
+# ---------------------------------------
+class Event(models.Model):
+	# Class variables
+	timestamp	= models.DateField(auto_now=512, auto_now_add=True)
+	status		= models.ForeignKey(Status)
+	service_id	= models.IntegerField()
+	description	= models.TextField()
+	
+	def __unicode__(self):
+		return self.name
+
