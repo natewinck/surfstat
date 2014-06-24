013627a8849e1e5ea66ae3d421d2c513a44e1a5e
natewinck <gotmacs@gmail.com>
2014-06-24 10:21:31 -0400

    UI Updates and Code Cleanup

--- js/effects.js
+++ js/effects.js
@@ -1,86 +1,3 @@
-// Options for the animated graphs
-var options = {
-    //Boolean - If we show the scale above the chart data			
-    scaleOverlay : false,
-
-    //Boolean - If we want to override with a hard coded scale
-    scaleOverride : false,
-
-    //** Required if scaleOverride is true **
-    //Number - The number of steps in a hard coded scale
-    scaleSteps : null,
-    //Number - The value jump in the hard coded scale
-    scaleStepWidth : null,
-    //Number - The scale starting value
-    scaleStartValue : null,
-
-    //String - Colour of the scale line	
-    scaleLineColor : "rgba(0,0,0,.1)",
-
-    //Number - Pixel width of the scale line	
-    scaleLineWidth : 1,
-
-    //Boolean - Whether to show labels on the scale	
-    scaleShowLabels : true,
-
-    //Interpolated JS string - can access value
-    scaleLabel : "<%=value%>",
-
-    //String - Scale label font declaration for the scale label
-    scaleFontFamily : "'Arial'",
-
-    //Number - Scale label font size in pixels	
-    scaleFontSize : 12,
-
-    //String - Scale label font weight style	
-    scaleFontStyle : "normal",
-
-    //String - Scale label font colour	
-    scaleFontColor : "#666",	
-
-    ///Boolean - Whether grid lines are shown across the chart
-    scaleShowGridLines : false,
-
-    //String - Colour of the grid lines
-    scaleGridLineColor : "rgba(0,0,0,.05)",
-
-    //Number - Width of the grid lines
-    scaleGridLineWidth : 1,	
-
-    //Boolean - Whether the line is curved between points
-    bezierCurve : true,
-
-    //Boolean - Whether to show a dot for each point
-    pointDot : true,
-
-    //Number - Radius of each point dot in pixels
-    pointDotRadius : 3,
-
-    //Number - Pixel width of point dot stroke
-    pointDotStrokeWidth : 1,
-
-    //Boolean - Whether to show a stroke for datasets
-    datasetStroke : true,
-
-    //Number - Pixel width of dataset stroke
-    datasetStrokeWidth : 2,
-
-    //Boolean - Whether to fill the dataset with a colour
-    datasetFill : true,
-
-    //Boolean - Whether to animate the chart
-    animation : true,
-
-    //Number - Number of animation steps
-    animationSteps : 60,
-
-    //String - Animation easing effect
-    animationEasing : "easeOutQuart",
-
-    //Function - Fires when the animation is complete
-    onAnimationComplete : null
-}
-
 $(function() {
 
 // SMOOTH SCROLLING
@@ -97,25 +14,72 @@ $("a[href*=#]:not([href=#])").click(function() {
     }
 });
 
-/* GRAPHS
-------------------- */
 
+/* CHARTS
+------------------------------ 
+*  Sets up all charts on a page with the .chart selector.
+*  Data is grabbed from the data attribute (FOR NOW)
+*  When the chart either comes in view or is unhidden,
+*  the chart fades in after a short delay.
+*  
+*  INPUT
+*  cavnas.chart
+*  canvas.chart[data]
+*  
+*  EVENT
+*  inview
+*  
+------------------------------ */
 // Set up each of the inview events.
-$(".graph").on("inview", function() {
+$("canvas.chart").on("inview", function() {
     var $this = $(this);
     $this.removeClass("hidden").off("inview");
     setTimeout(function() {
-        showGraph($this)
+        showLineChart($this)
     }, 250);
     //$this.fadeIn(1000);
-    $this.animate({ opacity: 1 }, 1000)
+    $this.animate({ opacity: 1 }, 1000);
 });
 
 
-function showGraph($this) {
+function showLineChart($this) {
     var ctx = $this[0].getContext("2d");
+    var data = chartData[$this.attr("data-id")];
+    //var data = $this.attr("data");
+    //data = $.parseJSON(data);
     
-    var data = {
+    
+    // If colors are not set for the datasets, give them default color schemes
+    for (var d in data.datasets) {
+        var dataset = data.datasets[d];
+        switch(Number(d)) {
+            // First dataset color scheme
+            case 0:
+                if (!dataset.fillColor) dataset.fillColor = "rgba(220,220,220,0.5)";
+                if (!dataset.strokColor) dataset.strokeColor = "rgba(220,220,220,1)";
+                if (!dataset.pointColor) dataset.pointColor = "rgba(220,220,220,1)";
+                if (!dataset.pointStrokeColor) dataset.pointStrokeColor = "#fff";
+                break;
+                
+            // Second dataset color scheme (layered on top of first)
+            case 1:
+                if (!dataset.fillColor) dataset.fillColor = "rgba(73,23,109,0.5)";
+                if (!dataset.strokColor) dataset.strokeColor = "rgba(52,12,75,1)";
+                if (!dataset.pointColor) dataset.pointColor = "rgba(52,12,75,1)";
+                if (!dataset.pointStrokeColor) dataset.pointStrokeColor = "#fff";
+                break;
+            
+            // Default dataset color scheme
+            default:
+                if (!dataset.fillColor) dataset.fillColor = "rgba(220,220,220,0.5)";
+                if (!dataset.strokColor) dataset.strokeColor = "rgba(220,220,220,1)";
+                if (!dataset.pointColor) dataset.pointColor = "rgba(220,220,220,1)";
+                if (!dataset.pointStrokeColor) dataset.pointStrokeColor = "#fff";
+        }     
+    }
+    
+    
+    var data2 = {
         labels : ["January","February","March","April","May","June","July"],
         datasets : [
             {
@@ -134,17 +98,86 @@ function showGraph($this) {
             }
         ]
     }
+    
+    // Options for the animated charts
+    var options = {
+        ///Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines : false,
 
+        //String - Scale label font declaration for the scale label
+        scaleFontFamily : "'Helvetica Neue', 'Arial'",
+
+        //Function - Fires when the animation is complete
+        onAnimationComplete : null
+    }
+    
     var chart = new Chart(ctx).Line(data, options);
 }
-    
-/* ----------------------------
-/* Tabbable Tabs
-/* ---------------------------- */
-$('#hangtime a').click(function (e) {
+
+
+
+
+
+/* TABBABLE TABS
+------------------------------ 
+*  Assigns a CLICK event to all .nav a elements that have a data-toggle attribute.
+*  The elements that have a corresponding id to the <a> href will show or hide.
+*  
+*  Pseudo example: <a href="#surfice"></a> will show <div id="surfice"></div>
+*  
+*  INPUT
+*  .nav a[data-toggle]          <a> elements that have a data-toggle attribute
+* 
+*  EVENT
+*  click
+*
+------------------------------ */
+$('.nav a[data-toggle]').click(function (e) {
     e.preventDefault()
     $(this).tab('show')
 })
+
+
+
+/* SURFICE BUTTONS
+------------------------------ 
+*  Assigns a mouseover and mouseout event to all .surfice elements.
+*  The first element inside .surfice will fade out when hovering over .surfice
+*  while the second element will fade in.  The opposite is true on mouseout
+*  
+*  INPUT
+* .surfice
+* 
+*  EVENT
+*  hover (mouseover and mouseout)
+*
+------------------------------ */
+
+$(".surfice").hover(
+    // Mouseover the Surfice element
+    function() {
+        // The First element of the Surfice is 
+        // what is supposed to be displayed always
+        // The Second element is hidden initially.
+        // When hovering over Surfice, stop any animation
+        // that might be happening (like fading in)
+        
+        $(this).children().first().stop().fadeOut(200);
+        $(this).children().last().stop().fadeIn(200);
+    },
+    
+    // Mouseout of the Surfice element
+    function() {
+        // When mousing out of a Surfice element,
+        // Fade out the second element and fade in the
+        // first (original) element.  But before that,
+        // Stop any animation that might be happening (so
+        // that it doesn't keep queueing up)
+        
+        $(this).children().last().stop().fadeOut(200);
+        $(this).children().first().stop().fadeIn(200);
+    }
+);
 
 
 });
