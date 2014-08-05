e4e50e2b3beebccf0a86787828b5f65a2426c395
natewinck <gotmacs@gmail.com>
2014-08-05 11:18:45 -0400

    Datetime updates

--- surfice/static/surfice/js/effects.js
+++ surfice/static/surfice/js/effects.js
@@ -689,28 +689,6 @@ $("table").on("click", 'tr[data-href] a[data-toggle="modal"]', function(e) {
 	return false;
 });
 
-function isInputTypeSupported(typeName) {
-    // Create element
-    var input = document.createElement("input");
-    // attempt to set the specified type
-    input.setAttribute("type", typeName);
-    // If the "type" property equals "text"
-    // then that input type is not supported
-    // by the browser
-    var val = (input.type !== "text");
-    // Delete "input" variable to
-    // clear up its resources
-    delete input;
-    // Return the detected value
-    return val;
-}
-
-if (!isInputTypeSupported("datetime-local")) {
-	$(".datetimepicker").datetimepicker();
-}
-
-});
-
 /* AJAX PROCESSES DISPLAY
 -------------------------------
 *  Pulse buttons, replace text, and perform other display functions
